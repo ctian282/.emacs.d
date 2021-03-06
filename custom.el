680637ad3bc8d0b148284a3184167b513122f3e6
Chi <rectaflex@gmail.com>
2021-03-05 22:03:07 -0500

    clear custom.el

--- custom.el
+++ custom.el
@@ -79,56 +79,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(centaur-package-archives 'melpa)
- '(centaur-theme 'default)
- '(custom-safe-themes
-   '("fce3524887a0994f8b9b047aef9cc4cc017c5a93a5fb1f84d300391fba313743" "2cdc13ef8c76a22daa0f46370011f54e79bae00d5736340a5ddfe656a767fddf" "8f5a7a9a3c510ef9cbb88e600c0b4c53cdcdb502cfe3eb50040b7e13c6f4e78e" "0a41da554c41c9169bdaba5745468608706c9046231bbbc0d155af1a12f32271" "76bfa9318742342233d8b0b42e824130b3a50dcc732866ff8e47366aed69de11" "f2927d7d87e8207fa9a0a003c0f222d45c948845de162c885bf6ad2a255babfd" "4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" "990e24b406787568c592db2b853aa65ecc2dcd08146c0d22293259d400174e37" default)))
+ )
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(aw-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 3.0))))
- '(aw-minibuffer-leading-char-face ((t (:inherit font-lock-keyword-face :bold t :height 2.0))))
- '(aw-mode-line-face ((t (:inherit mode-line-emphasis :bold t))))
- '(dashboard-heading ((t (:inherit (font-lock-string-face bold)))))
- '(diff-hl-change ((t (:foreground "#51afef" :background nil))))
- '(diff-hl-delete ((t (:background nil))))
- '(diff-hl-insert ((t (:background nil))))
- '(doom-modeline-buffer-file ((t (:inherit (mode-line bold)))))
- '(git-timemachine-minibuffer-author-face ((t (:inherit success))))
- '(git-timemachine-minibuffer-detail-face ((t (:inherit warning))))
- '(ivy-minibuffer-match-face-1 ((t (:inherit font-lock-doc-face :foreground nil))))
- '(lsp-headerline-breadcrumb-path-error-face ((t :underline (:style wave :color "#ff6c6b") :inherit lsp-headerline-breadcrumb-path-face)))
- '(lsp-headerline-breadcrumb-path-hint-face ((t :underline (:style wave :color "#98be65") :inherit lsp-headerline-breadcrumb-path-face)))
- '(lsp-headerline-breadcrumb-path-info-face ((t :underline (:style wave :color "#98be65") :inherit lsp-headerline-breadcrumb-path-face)))
- '(lsp-headerline-breadcrumb-path-warning-face ((t :underline (:style wave :color "#ECBE7B") :inherit lsp-headerline-breadcrumb-path-face)))
- '(lsp-headerline-breadcrumb-symbols-error-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#ff6c6b"))))
- '(lsp-headerline-breadcrumb-symbols-hint-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#98be65"))))
- '(lsp-headerline-breadcrumb-symbols-info-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#98be65"))))
- '(lsp-headerline-breadcrumb-symbols-warning-face ((t :inherit lsp-headerline-breadcrumb-symbols-face :underline (:style wave :color "#ECBE7B"))))
- '(lsp-ui-sideline-code-action ((t (:inherit warning))))
- '(macrostep-expansion-highlight-face ((t (:background "#23272e" :extend t))))
- '(org-ellipsis ((t (:foreground nil))))
- '(org-pomodoro-mode-line ((t (:inherit warning))))
- '(org-pomodoro-mode-line-break ((t (:inherit success))))
- '(org-pomodoro-mode-line-overtime ((t (:inherit error))))
- '(pulse-highlight-face ((t (:inherit region))))
- '(pulse-highlight-start-face ((t (:inherit region))))
- '(symbol-overlay-default-face ((t (:inherit (region bold)))))
- '(ztreep-arrow-face ((t (:inherit font-lock-comment-face))))
- '(ztreep-diff-header-face ((t (:inherit (diff-header bold)))))
- '(ztreep-diff-header-small-face ((t (:inherit diff-file-header))))
- '(ztreep-diff-model-add-face ((t (:inherit diff-nonexistent))))
- '(ztreep-diff-model-diff-face ((t (:inherit diff-removed))))
- '(ztreep-diff-model-ignored-face ((t (:inherit font-lock-doc-face :strike-through t))))
- '(ztreep-diff-model-normal-face ((t (:inherit font-lock-doc-face))))
- '(ztreep-expand-sign-face ((t (:inherit font-lock-function-name-face))))
- '(ztreep-header-face ((t (:inherit diff-header))))
- '(ztreep-leaf-face ((t (:inherit diff-index))))
- '(ztreep-node-face ((t (:inherit font-lock-variable-name-face)))))
-
+ )
 
 ;;; custom.el ends here
 
